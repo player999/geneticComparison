1d256b311e85e990ee34b86264fd382dea1a8080
Taras Zakharchenko <taras.zakharchenko@gmail.com>
2017-05-02 19:52:03 +0300

    Added functions up to F20

--- create_test_function.m
+++ create_test_function.m
@@ -5,6 +5,18 @@ function f = create_test_function(fname)
     if strcmp(fname, 'F16') == 1
         f = struct('func', @create_function_16, 'xlim', [0,1], 'peaks', @peaks15);
     end
+    if strcmp(fname, 'F18') == 1
+        f = struct('func', @create_function_18, 'xlim', [0,1], 'peaks', @peaks18);
+    end
+    if strcmp(fname, 'F19') == 1
+        f = struct('func', @create_function_19, 'xlim', [0,1], 'peaks', @peaks18);
+    end
+    if strcmp(fname, 'F20min') == 1
+        f = struct('func', @create_function_20min, 'xlim', [-5.12,5.12], 'peaks', @peaks20min);
+    end
+    if strcmp(fname, 'F20max') == 1
+        f = struct('func', @create_function_20max, 'xlim', [-5.12,5.12], 'peaks', @peaks20max);
+    end
 end
 
 function rval = create_function_15(args)
@@ -22,10 +34,66 @@ function rval = create_function_16(args)
     end
 end
 
+function rval = create_function_18(args)
+    rval = 0;
+    for i = 1:numel(args)
+       rval = rval + sin(5 * pi * (args(i)^0.75 - 0.05))^6;
+    end
+    rval = rval / numel(args);
+end
+
+function rval = create_function_19(args)
+    rval = 0;
+    for i = 1:numel(args)
+       rval = rval + exp(-2 * log(2)*((args(i)-0.08)/0.854)^2) * sin(5 * pi * (args(i)^0.75 - 0.05))^6;
+    end
+    rval = rval / numel(args);
+end
+
+function rval = create_function_20min(args)
+    rval = 10 * numel(args);
+    for i = 1:numel(args)
+       rval = rval + args(i)^2 - 10 * cos(2 * pi * args(i));
+    end
+    rval = rval / numel(args);
+end
+
+function rval = create_function_20max(args)
+    rval = -10 * numel(args);
+    for i = 1:numel(args)
+       rval = rval - args(i)^2 + 10 * cos(2 * pi * args(i));
+    end
+    rval = rval / numel(args);
+end
+
 function rval = peaks15(nargs)
     vectors = {};
     for i=1:nargs
        vectors{i} = [0.1:0.2:0.9];
+    end
+    rval = combvec(vectors{:})';
+end
+
+function rval = peaks18(nargs)
+    vectors = {};
+    for i=1:nargs
+       vectors{i} = arrayfun(@(x)(x^(4/3)), [0.15:0.2:0.95]);
+    end
+    rval = combvec(vectors{:})';
+end
+
+function rval = peaks20min(nargs)
+    vectors = {};
+    for i=1:nargs
+       vectors{i} = [-4.52299 -3.51786 -2.51274 -1.50764 -0.502546 0.502546 1.50764 2.51274 3.51786 4.52299];
+    end
+    rval = combvec(vectors{:})';
+end
+
+function rval = peaks20max(nargs)
+    vectors = {};
+    for i=1:nargs
+       vectors{i} = [-3.97978 -2.98486 -1.98991 -0.994959 0 0.994959 1.98991 2.98486 3.97978];
     end
     rval = combvec(vectors{:})';
 end
--- make_populations.m
+++ make_populations.m
@@ -3,7 +3,7 @@ function pops = make_populations(popsize, nargs, lims, runs)
     for i=1:runs
         pop = rand(popsize, nargs);
         for j=1:nargs
-            pop(:,j) = (pop(:,j) - lims(j,1)) ./ (lims(j,2) - lims(j,1));
+            pop(:,j) = (pop(:,j) .* (lims(j,2) - lims(j,1))) + lims(j,1);
             pops(:,:,i) = pop;
         end
     end
