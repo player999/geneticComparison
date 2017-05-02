12cc5a05783e483d06c3b09166e8019c1e1582b5
Taras Zakharchenko <taras.zakharchenko@gmail.com>
2017-05-02 21:59:21 +0300

    Write xlsx fix

--- write_excel_result.m
+++ write_excel_result.m
@@ -1,10 +1,10 @@
 function write_excel_result(results, params)
     resfile = sprintf('result_%s.xlsx', datestr(datetime,'dd_MM_yyyy_HH_mm_ss')); 
     copyfile('result_template.xlsx', resfile);
-    xlswrite(resfile, results.per_run{1}.nfe, 'A6');%Run1 NFE
-    xlswrite(resfile, results.per_run{1}.pn, 'B6');%Run1 NP
-    xlswrite(resfile, results.per_run{1}.pr, 'C6');%Run1 PR
-    xlswrite(resfile, results.per_run{1}.pa, 'D6');%Run1 PA
-    xlswrite(resfile, results.per_run{1}.da, 'E6');%Run1 DA
+    xlswrite(resfile, results.per_run{1}.nfe, 1, 'A6');%Run1 NFE
+    xlswrite(resfile, results.per_run{1}.pn, 1, 'B6');%Run1 NP
+    xlswrite(resfile, results.per_run{1}.pr, 1, 'C6');%Run1 PR
+    xlswrite(resfile, results.per_run{1}.pa, 1, 'D6');%Run1 PA
+    xlswrite(resfile, results.per_run{1}.da, 1, 'E6');%Run1 DA
     
 end
