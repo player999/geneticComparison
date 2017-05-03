427331301fe856f1e56b34a33ba75c76ef670b26
Taras Zakharchenko <taras.zakharchenko@gmail.com>
2017-05-03 22:10:33 +0300

    Removed extra line in XLSX

--- mat2xlsx.m
+++ mat2xlsx.m
@@ -2,5 +2,5 @@ function mat2xlsx(matname, xlsname)
     copyfile('result_template2.xlsx', xlsname);
     load(matname, 'res_matrix');
     lines_count = size(res_matrix, 1);
-    xlswrite(xlsname, res_matrix, 1, sprintf('A6:BC%d', lines_count + 6));
+    xlswrite(xlsname, res_matrix, 1, sprintf('A6:BC%d', lines_count + 5));
 end
